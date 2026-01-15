b41964a2783887bfb99b8335e9f5f65a8cd5a4b7
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2026-01-15 19:47:47 -0300

    Criação do Menu para o usuario poder selecionar a sua opção desejada...

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿
+﻿static void Menu()
+{
+    Console.WriteLine("[0] - Sair do Programa");
+    Console.WriteLine("[1] - Adicionar Aluno");
+    Console.WriteLine("[2] - Adicionar Notas");
+    Console.WriteLine("[3] - Media do Aluno");
+    Console.WriteLine("[4] - Status do Aluno");
+}
 
+Menu();
